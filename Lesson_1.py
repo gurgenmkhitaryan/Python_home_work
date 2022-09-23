9b010e8f0ac3236d9e2328203eb2a23f17b1d66f
rico <gugomkh@mail.ru>
2022-09-23 23:08:19 +0300

    ДЗ к первому уроку

---
+++ Lesson_1.py
@@ -0,0 +1,8 @@
+num_1 = int(input('Введите первую цыфру '))
+num_1 = int(input('Введите первую цыфру '))
+num_1 = int(input('Введите первую цыфру '))
+num_1 = int(input('Введите первую цыфру '))
+num_1 = int(input('Введите первую цыфру '))
+max_number = 0
+if num_1 > num_2:
+    max_number
