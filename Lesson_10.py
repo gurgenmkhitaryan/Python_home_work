6fcd6b8dbdef34bd260a7aeeada764951bfd5702
rico <gugomkh@mail.ru>
2022-10-05 07:47:39 +0300

    ДЗ к третьему уроку

---
+++ Lesson_10.py
@@ -0,0 +1,17 @@
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+# Пример:
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+def new_list(size_):
+    new_list = []
+    for i in range(1, size_ + 1):
+        new_list.append(i)
+    return new_list
+
+
+my_list = new_list(int(input('Введите размер списка: ')))
+count = 0
+for i in range(1, len(my_list), 2):
+        count += i
+        print('i =', i)
+print('Сумма нечётных позиции =', count)
