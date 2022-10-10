5f798ecec2f3f8aac56b783076f9a57eb7c07593
rico <gugomkh@mail.ru>
2022-10-10 08:23:06 +0300

    ДЗ к четвертому уроку

---
+++ Lesson_18.py
@@ -0,0 +1,35 @@
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Пример:
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+import itertools
+from random import randint
+
+print('Чтобы сформировать многочлен степени k и записать в файл, введите степень k! ')
+k = int(input("Введите степень k: "))
+
+factor = []
+for i in range(1, k + 2):
+    factor.append(randint(1, 101))
+
+result = []
+for i in range(len(factor)):
+    if k == 1:
+        result.append(f'{factor[i]}*x')
+    elif k == 0:
+        result.append(f'{factor[i]}')
+    else:
+        result.append(f'{factor[i]}*x^{k}')
+    signs = randint(0, 1)
+    if signs == 1:
+        result.append('+')
+    elif signs == 0:
+        result.append('-')
+    k -= 1
+
+result.pop(-1)
+result.append('=0')
+
+record = open('data.txt', 'w')
+record.write(''.join(result))
+record.close()
